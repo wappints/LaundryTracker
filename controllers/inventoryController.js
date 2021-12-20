deb4c89dc962462c4d42494b80802e165e854e24
jiyero <jihro_abendano@dlsu.edu.ph>
2021-12-20 13:42:28 +0800

    fix nav drop down and added inventoryController skeleton

---
+++ controllers/inventoryController.js
@@ -0,0 +1,13 @@
+// import module `database` from `../models/db.js`
+const { request } = require('express');
+const db = require('../models/db.js');
+
+// import module `System` from `../models/SystemModel.js`
+const Sale = require('../models/SalesModel.js');
+
+const inventoryController = {
+    
+}
+
+
+module.exports = inventoryController;
