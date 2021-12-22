47209b46aad4d112a5b88c468bae0137780708ea
Yeohan Norona <realmsoflorenzo@gmail.com>
2021-12-23 05:06:44 +0800

    finished Accounts

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -19,24 +19,27 @@ const accountsController = {
             {
                 if(i.isAdmin)
                 {
+                    obj = {}
                     obj["AccID"] = i._id
                     obj["isAdmin"] = i.isAdmin
                     obj["EMPName"] = i.EMPName
                     obj["EMPPass"] = i.EMPPass
                     details.push(obj)
-                    console.log(obj)
                 }
                 else
                 {
+                    obj2 = {}
                     obj2["AccID"] = i._id
                     obj2["isAdmin"] = i.isAdmin
                     obj2["EMPName"] = i.EMPName
                     obj2["EMPPass"] = i.EMPPass
                     details2.push(obj2)
-                    console.log(obj2)
                 }
-                
+
             }
+            console.log("THE DETAILS")
+            console.log(details)
+            console.log(details2)
             res.render("management", {admin : details, accounts : details2, layout : "managementLayout", ACCType : ACCType, DDate : DDate})
         })
 
@@ -45,9 +48,11 @@ const accountsController = {
     updateAccounts : function (req,res) {
         var ACCType = req.params.ACCType
         var DDate = req.params.DDate
-        var EMPName = req.body.EMPName
-        var EMPPass = req.body.EMPPass
+        var EMPName = req.body.Name
+        var EMPPass = req.body.Pass
         var ID = req.body.ID
+        const { ObjectId } = require('mongodb');
+        ID = ObjectId(ID);   
         console.log(ID)
         console.log("FINAL")
         db.updateOne(Account, {_id : ID}, {EMPName : EMPName, EMPPass : EMPPass}, function(result)
@@ -64,23 +69,23 @@ const accountsController = {
         db.deleteOne(Account, {_id : ID}, function(result)
         
         {
-           
+            res.redirect("back")
         })
         
 
     },
         addAccount : function (req,res) {
-        var EMPName = req.body.EMPName
-        var EMPPass = req.body.EMPPass
+        var EMPName = req.body.Name
+        var EMPPass = req.body.Pass
         var docs = {
             EMPName : EMPName,
             EMPPass : EMPPass,
             isAdmin : false
         }
-
+        console.log(docs)
         db.insertOne(Account, docs, function(result)
         {
-           
+           res.redirect("back")
         })
         
 
