e74f95453c0699836c678db009f25cfc59b3c163
Yeohan Norona <realmsoflorenzo@gmail.com>
2022-01-03 18:33:28 +0800

    if import becomes string

--- controllers/editPriceController.js
+++ controllers/editPriceController.js
@@ -15,9 +15,24 @@ const editPriceController = {
             SOAPPrice : parseInt(req.body.ESOAPPrice),
             DOWNPrice : parseInt(req.body.EDOWNPrice),
         }
-        db.updateOne(Price, {key:docs.key}, docs, function(result){
-            res.redirect("back")
+        db.findOne(Price, {key:docs.key}, docs, function(result){
+            if (typeof result.TNWPrice === 'string' ) {
+                docs = {   
+                    key : 'Price',
+                    TNWPrice : req.body.ETNWPrice.toString(),
+                    TNDPrice : req.body.ETNDPrice.toString(),
+                    TKWPrice : req.body.ETKWPrice.toString(),
+                    TKDPrice : req.body.ETKDPrice.toString(),
+                    FOLDrice : req.body.EFOLDPrice.toString(),
+                    SOAPPrice : req.body.ESOAPPrice.toString(),
+                    DOWNPrice : req.body.EDOWNPrice.toString(),
+                }
+            }
+            db.updateOne(Price, {key:docs.key}, docs, function(result){
+                res.redirect("back")
+            })
         })
+
     }
     
 }
