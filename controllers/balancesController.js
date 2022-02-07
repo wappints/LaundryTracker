917fe2c88f0b06dd7e2419d86e6fc95687675703
Yeohan Norona <realmsoflorenzo@gmail.com>
2022-02-07 23:52:35 +0800

    added log for deleting the entry EXACTLY FROM BALANCE PAGE

--- controllers/balancesController.js
+++ controllers/balancesController.js
@@ -90,9 +90,63 @@ const balancesController = {
     deleteBalance : function(req,res)
     {
         var BalanceID = req.query.card
+        var ACCType = req.query.ACCType
+        var Session = req.query.Session
+
         if (BalanceID != null) {
             db.deleteOne(Sale, {_id : BalanceID}, function(result){
                 if (result != null) {
+                    db.findOne(Balances, {BalanceID: BalanceID}, {}, function(result){
+                        var temp = new Date()
+                        temp.setHours(temp.getHours() + 8);
+                        var hour = temp.getHours()
+                        hour=hour-8;
+                        if (hour<0)
+                            hour=hour+24 
+                        var minutes = temp.getMinutes()
+                        if (minutes < 10){
+                            minutes = ("0"+minutes);
+                        }
+                        var seconds = temp.getSeconds()
+                        if (seconds < 10){
+                            seconds = ("0"+seconds);
+                        }
+                        var time = hour + ":" + minutes + ":" + seconds
+                        if (result != null) {
+                            var EditLog = [ACCType + " " + Session + " deleted a balance parent entry"]
+                            var EditLog2 = "NOTE: ENTRY DELETED FROM BALANCES";
+                            var Handler = [ time ]
+                            var Handler2 = time
+                            var mongoose = require('mongoose');
+
+                            db.findOne(Log, {LogID :BalanceID}, {}, function(result){
+                                var new1 = result.EditLog 
+                                new1.push("\n" + EditLog2)
+                                new1.push("\n" + "RELATED PARTIAL PAYMENT ENTRIES ALSO DELETED")
+                                var new2 = result.Handler
+                                new2.push("\n" + Handler2)
+                                new2.push("\n" + Handler2)
+                                
+                                db.updateOne(Log, {LogID:BalanceID}, {EditLog:new1}, function(result){})
+                                db.updateOne(Log, {LogID:BalanceID}, {Handler:new2}, function(result){})
+                            })
+                            var ID = new mongoose.mongo.ObjectId();
+                            var docss = {
+                                LogID : ID,
+                                Name : result.Name,
+                                TotalPrice : result.Balance,
+                                AmountPaid : 0,
+                                Balance : result.Balance,
+                                TokenError : 0,
+                                EditLog : EditLog,
+                                Handler : Handler,
+                                DDate : temp,
+                            }
+                            
+                            db.insertOne(Log, docss, function(result){
+                            })
+                        }
+                    })
                     db.deleteOne(Balances, {BalanceID : BalanceID}, function(result){
                         if (result != null) {
                             db.deleteMany(Sale, {BalanceID : BalanceID}, function (result) {})
